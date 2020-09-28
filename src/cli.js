15bba793431f8a1c7148534ac8b0ce5ad9d65dbd
Evgeny Tevelevich <eyt5297@gmail.com>
2020-09-28 17:54:41 +0700

    Step 1: auto generate help

--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,11 @@
+import pkg from 'commander';
+
+const { program } = pkg;
+
 const gendiff = () => {
-  console.log('test gendiff');
+  program.version('0.0.1');
+
+  program.parse(process.argv);
 };
 
 export default gendiff;
--- src/index.js
+++ src/index.js
@@ -3,4 +3,3 @@ const getDiffs = () => {
 };
 
 export default getDiffs;
-
