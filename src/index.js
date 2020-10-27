cc74c9776b59491e75d2b47bd03bac56dc506d86
Evgeny Tevelevich <eyt5297@gmail.com>
2020-10-27 19:20:36 +0700

    add function readJSON

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import fs from 'fs';
 
+const readJSON = (file) => {
+  const contentFile = fs.readFileSync(file, 'utf8');
+  return JSON.parse(contentFile);
+};
+
+
 const genDiff = (file1, file2) => {
-  const contentFile1 = fs.readFileSync(file1);
-  const contentFile2 = fs.readFileSync(file2);
-  console.log(contentFile1);
-  console.log(contentFile2);
-  console.log('do...');
+  const json1 = readJSON(file1);
+  const json2 = readJSON(file2);
+
+  console.log(json1);
+  console.log(json2);
 };
 
 export default genDiff;
