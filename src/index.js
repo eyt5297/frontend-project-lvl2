f8ea7b292800fac56e7c47c893606a90c39ce0a6
Evgeny Tevelevich <eyt5297@gmail.com>
2021-07-22 03:57:28 +0700

    Step 6 add support diff tree

--- src/index.js
+++ src/index.js
@@ -6,6 +6,13 @@ const getStatusKeys = (obj1, obj2) => {
   const keys2 = Object.keys(obj2);
   const allKeys = _.union(keys1, keys2).sort();
 
+  const getValue = (objValue) => {
+    if (_.isPlainObject(objValue)) {
+      return getStatusKeys(objValue, objValue);
+    }
+    return objValue;
+  };
+
   const statusKeys = allKeys.map((key) => {
     const diffKey = { key };
     const isIncludeKeys1 = _.includes(keys1, key);
@@ -13,18 +20,24 @@ const getStatusKeys = (obj1, obj2) => {
 
     if (!isIncludeKeys1) {
       diffKey.status = 'add';
-      diffKey.newValue = obj2[key];
+      diffKey.newValue = getValue(obj2[key]);
       return diffKey;
     }
 
     if (!isIncludeKeys2) {
       diffKey.status = 'remove';
-      diffKey.value = obj1[key];
+      diffKey.value = getValue(obj1[key]);
       return diffKey;
     }
 
-    diffKey.value = obj1[key];
-    diffKey.newValue = obj2[key];
+    if (_.isPlainObject(obj1[key]) && _.isPlainObject(obj2[key])) {
+      diffKey.value = getStatusKeys(obj1[key], obj2[key]);
+      diffKey.status = 'leav';
+      return diffKey;
+    }
+
+    diffKey.value = getValue(obj1[key]);
+    diffKey.newValue = getValue(obj2[key]);
 
     if (diffKey.value === diffKey.newValue) {
       diffKey.status = 'leav';
@@ -38,27 +51,36 @@ const getStatusKeys = (obj1, obj2) => {
   return statusKeys;
 };
 
-const getResultDiff = (diffKeys) => {
+const getResultDiff = (diffKeys, shiftCount = 1) => {
+  const convertValue = (value) => {
+    if (_.isArray(value)) {
+      return getResultDiff(value, shiftCount + 2);
+    }
+    return value;
+  };
+
   const diffText = diffKeys
-    .map((diffKey) => {
+    .flatMap((diffKey) => {
       switch (diffKey.status) {
         case 'add':
-          return `  + ${diffKey.key}: ${diffKey.newValue}`;
+          return `${'  '.repeat(shiftCount)}+ ${diffKey.key}: ${convertValue(diffKey.newValue)}`;
         case 'remove':
-          return `  - ${diffKey.key}: ${diffKey.value}`;
+          return `${'  '.repeat(shiftCount)}- ${diffKey.key}: ${convertValue(diffKey.value)}`;
         case 'leav':
-          return `    ${diffKey.key}: ${diffKey.value}`;
+          return `${'  '.repeat(shiftCount)}  ${diffKey.key}: ${convertValue(diffKey.value)}`;
+        case 'change':
+          return `${'  '.repeat(shiftCount)}- ${diffKey.key}: ${convertValue(diffKey.value)}
+${'  '.repeat(shiftCount)}+ ${diffKey.key}: ${convertValue(diffKey.newValue)}`;
         default:
           break;
       }
-      return `  - ${diffKey.key}: ${diffKey.value}
-  + ${diffKey.key}: ${diffKey.newValue}`;
+      return null;
     })
     .join('\n');
 
   return `{
 ${diffText}
-}`;
+${'  '.repeat(shiftCount - 1)}}`;
 };
 
 const genDiff = (file1, file2) => {
@@ -66,6 +88,7 @@ const genDiff = (file1, file2) => {
   const obj2 = parsers(file2);
 
   const diffKeys = getStatusKeys(obj1, obj2);
+  // console.log(JSON.stringify(diffKeys, undefined, 4));
   const diffText = getResultDiff(diffKeys);
 
   return diffText;
